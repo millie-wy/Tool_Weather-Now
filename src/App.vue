cf944b229e56cc04352fd201f54a6f2d15986ff2
Millie Cheung <cheungmillie9@gmail.com>
2023-07-18 13:50:57 +0200

    setup data store and passed down from app to components

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
 <script setup>
 import { RouterView } from 'vue-router'
 import HeaderView from './HeaderView.vue'
+import { useDataStore } from './store/DataStore'
+
+const store = useDataStore()
 </script>
 
 <template>
-  <HeaderView />
-  <RouterView />
+  <HeaderView :tempScale="store.$state.tempScale" />
+  <RouterView :data="store.$state" />
 </template>
