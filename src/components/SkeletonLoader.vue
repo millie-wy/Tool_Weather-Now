94837cbb0b8692124da61df3a12c8fe9d91028ab
Millie Cheung <cheungmillie9@gmail.com>
2023-07-19 20:07:38 +0200

    added skeleton loader in HomeView

---
+++ src/components/SkeletonLoader.vue
@@ -0,0 +1,41 @@
+<script>
+export default {
+  props: {
+    height: Number,
+    width: Number
+  },
+  computed: {
+    cssProps() {
+      return {
+        '--height': this.height ? this.height - 5 + 'px' : '14.5px',
+        '--width': this.width ? this.width + 'px' : '200px'
+      }
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="half-transparent" :style="cssProps"></div>
+</template>
+
+<style scoped>
+div {
+  width: var(--width);
+  height: var(--height);
+  margin-top: 5px;
+  animation: pulse-bg 1s infinite;
+}
+
+@keyframes pulse-bg {
+  0% {
+    background: #ffffff60;
+  }
+  50% {
+    background: #d6d6d660;
+  }
+  100% {
+    background: #ffffff60;
+  }
+}
+</style>
--- src/pages/HomeView.vue
+++ src/pages/HomeView.vue
@@ -1,73 +1,103 @@
 <script>
 import FooterView from '../FooterView.vue'
+import SkeletonLoader from '../components/SkeletonLoader.vue'
 
+//TODO: local storage
 export default {
   props: {
     data: {
-      weatherData: Object,
+      weatherData: Object, // TODO
       tempScale: String,
-      location: String
+      location: String // TODO
     }
   },
 
   components: {
-    FooterView
+    FooterView,
+    SkeletonLoader
   },
   computed: {
     tempScaleSymbol() {
       return this.data.tempScale === 'celsis' ? '°C' : '°F'
+    },
+    hasWeatherData() {
+      return Object.keys(this.data.weatherData).length > 0
     }
   }
 }
 </script>
+
 <template>
   <div class="page-container">
-    <h4 class="green">Västra Götaland, Sweden</h4>
-    <h1>Gothenburg</h1>
-    <img src="../assets/weather-icons/176.png" alt="weather icon!!" class="w-icon" />
-    <h4>Partly cloudy</h4>
-    <p class="sm-text">Feels like 18{{ tempScaleSymbol }}</p>
-    <h2>18{{ tempScaleSymbol }}</h2>
+    <h4 v-if="hasWeatherData" class="green">Västra Götaland, Sweden</h4>
+    <SkeletonLoader v-else />
+
+    <h1 v-if="hasWeatherData">{{ data.location }}</h1>
+    <!-- TODO -->
+    <SkeletonLoader v-else height="39" />
+
+    <img
+      v-if="hasWeatherData"
+      src="../assets/weather-icons/176.png"
+      alt="weather icon!!"
+      class="w-icon"
+    />
+    <SkeletonLoader v-else width="64" height="64" class="w-icon" />
+    <h4 v-if="hasWeatherData">Partly cloudy</h4>
+    <SkeletonLoader v-else width="100" />
+    <p v-if="hasWeatherData" class="sm-text">Feels like 18{{ tempScaleSymbol }}</p>
+    <SkeletonLoader v-else width="100" />
+    <h2 v-if="hasWeatherData">18{{ tempScaleSymbol }}</h2>
+    <SkeletonLoader v-else width="100" height="45" />
 
     <div class="details row">
       <div class="d-tab">
         <h4 class="green label sm-text">UV Index</h4>
-        <p>4 Moderate</p>
+        <p v-if="hasWeatherData">4 Moderate</p>
+        <SkeletonLoader v-else width="90" />
       </div>
       <div class="d-tab">
         <h4 class="green label sm-text">Wind</h4>
-        <p>WSW 19.1 km/h</p>
+        <p v-if="hasWeatherData">WSW 19.1 km/h</p>
+        <SkeletonLoader v-else width="90" />
       </div>
       <div class="d-tab">
         <h4 class="green label sm-text">Wind Gust</h4>
-        <p>20.2 km/h</p>
+        <p v-if="hasWeatherData">20.2 km/h</p>
+        <SkeletonLoader v-else width="90" />
       </div>
       <div class="d-tab">
         <h4 class="green label sm-text">Cloud Cover</h4>
-        <p>50%</p>
+        <p v-if="hasWeatherData">50%</p>
+        <SkeletonLoader v-else width="90" />
       </div>
       <div class="d-tab">
         <h4 class="green label sm-text">Visibility</h4>
-        <p>10 km</p>
+        <p v-if="hasWeatherData">10 km</p>
+        <SkeletonLoader v-else width="90" />
       </div>
       <div class="d-tab">
         <h4 class="green label sm-text">Humidity</h4>
-        <p>73%</p>
+        <p v-if="hasWeatherData">73%</p>
+        <SkeletonLoader v-else width="90" />
       </div>
     </div>
 
     <div class="forcast-div row">
       <div class="f-col align-right">
         <h4>Tomorrow</h4>
-        <h4>15/7</h4>
+        <h4 v-if="hasWeatherData">15/7</h4>
+        <SkeletonLoader v-else width="75" />
       </div>
       <div class="f-col">
         <img src="../assets/weather-icons/176.png" alt="weather icon!!" width="20px" />
         <img src="../assets/weather-icons/176.png" alt="weather icon!!" width="20px" />
       </div>
       <div class="f-col">
-        <p>12 - 24{{ tempScaleSymbol }}</p>
-        <p>12.9 - 19.8{{ tempScaleSymbol }}</p>
+        <p v-if="hasWeatherData">12 - 24{{ tempScaleSymbol }}</p>
+        <SkeletonLoader v-else width="75" />
+        <p v-if="hasWeatherData">12.9 - 19.8{{ tempScaleSymbol }}</p>
+        <SkeletonLoader v-else width="75" />
       </div>
     </div>
   </div>
